19fc3a40493f70b4ca6d6e57de1bead10af6fc68
emanuel <emanuel@startapp.one>
2020-11-21 15:04:56 -0300

    search music works

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const bot = new Client();
 const token = require("./token.json");
 
 const ytdl = require("ytdl-core");
+const puppeteer = require("puppeteer");
 
 const PREFIX = "$";
 
@@ -16,7 +17,13 @@ bot.on("message", message => {
     let args = message.content.substring(PREFIX.length).split(" ");
 
     switch (args[0]) {
+
+        case "commands":
+            message.channel.send("Comandos disponíveis: $play // $skip // $stop // $leave // $ará")
+        break;
+
         case "play":
+            message.channel.send("Por favor, aguarde...");
 
             function play (connection, message){
                 let server = servers[message.guild.id];
@@ -32,6 +39,19 @@ bot.on("message", message => {
                         connection.disconnect();
                     }
                 });
+            }
+
+            async function scrapeProduct(url){
+                const browser = await puppeteer.launch();
+                const page = await browser.newPage();
+                await page.goto(url);
+            
+                const [el] = await page.$x('//*[@id="video-title"]');
+                const src = await el.getProperty("href");
+                const srcTxt = await src.jsonValue();
+            
+                browser.close();
+                return srcTxt;
             }
 
             if(!args[1]){
@@ -50,40 +70,71 @@ bot.on("message", message => {
 
             let server = servers[message.guild.id];
 
-            server.queue.push(args[1]);
+            let ytbUrl = args[1].split("=");
+            
+            if(ytbUrl[0] === "https://www.youtube.com/watch?v") {
+                server.queue.push(args[1]);
+                message.channel.send("Sua música foi adicionada na fila!");
+            }else{
+                args.shift();
+                let musicName = args.join("+");
+                let searchUrl = `https://www.youtube.com/results?search_query=${musicName}`;
+                scrapeProduct(searchUrl).then((result) => {
+                    server.queue.push(result);
+                    message.channel.send(`Coloquei ${result} na fila`);
+                 })
+               
+            }
 
-            if(!message.guild.me.voice.connection) message.member.voice.channel.join().then( ( connection ) => {
-                play(connection, message);
-            });
+            const startMusic = () => {
+                if(!message.guild.me.voice.connection) message.member.voice.channel.join().then( ( connection ) => {
+                    play(connection, message);
+                });
+            }
+            setTimeout(startMusic, 5000)
         break;
 
         case "skip":
             let serverSkip = servers[message.guild.id];
 
-            if(serverSkip.dispatcher) serverSkip.dispatcher.end();
-            message.channel.send("Pulou uma música.");
+            if(message.guild.voice && message.guild.me.voice ){
+                if(serverSkip){
+                    if(serverSkip.dispatcher) serverSkip.dispatcher.end();
+                    message.channel.send("Pulou uma música.");
+                }else{
+                    message.channel.send("Não tem nada pra pular.");
+                }
+            }else{
+                message.channel.send("ME COLOCA NA SUA CALL!!!!!!!");
+            }
 
         break;
 
         case "stop":
             let serverStop = servers[message.guild.id];
 
-            if(message.guild.voice){
+            if(message.guild.voice && message.guild.me.voice ){
                 if(message.guild.voice.connection){
                     for(let i = serverStop.queue.length -1; i>= 0; i--){
                         serverStop.queue.splite(i, 1);
                     }
                     message.channel.send("Fim da lista. Estou saindo do chat de voz.");
-                    serverStop.dispatcher.end()    
-                    console.log("stopped the queue");
+                    serverStop.dispatcher.end()
                 }
+            }else{
+                message.channel.send("ME COLOCA NA SUA CALL!!!!!!!");
             }
             
         break;
 
         case "leave":
-            if(message.guild.voice.connection) message.guild.voice.connection.disconnect();
+            if(message.guild.voice){
+                if(message.guild.voice.connection) message.guild.voice.connection.disconnect();
+            }else{
+                message.channel.send("ME COLOCA NA CALL!!!!!!!");
+            }
         break;
+
     }
 });
 
