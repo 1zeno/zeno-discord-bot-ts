1fcbe859add9dc98966b315b03d9510f3a5b744d
Emanuel <emanuel@startapp.one>
2021-08-02 00:10:47 -0300

    fixes

--- dist/token.js
+++
@@ -1,5 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.token = void 0;
-exports.token = "NTc3NzI0NTA0MjM1ODM1NDEy.XNpN3w.D9ByfEcGHf4ly5UB6Ha4ZP1k--4";
-//# sourceMappingURL=token.js.map
